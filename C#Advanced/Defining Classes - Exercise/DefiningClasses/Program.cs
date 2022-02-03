6e5210841b7253cf089e4ab70c43cc2ff5128ce2
IvailoSpasov <ivailo.andreev.spasov@gmail.com>
2022-02-03 21:09:43 +0200

    new exercises

--- C#Advanced/Defining Classes - Exercise/DefiningClasses/Program.cs
+++ C#Advanced/Defining Classes - Exercise/DefiningClasses/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading.Channels;
+using System.Linq;
 
 namespace DefiningClasses
 {
@@ -8,12 +8,129 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, Engine> engines = new Dictionary<string, Engine>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Engine engine = new Engine();
+                if (input.Length == 4)
+                {
+                    engine.Model = input[0];
+                    engine.Power = int.Parse(input[1]);
+                    engine.Displacement = int.Parse(input[2]);
+                    engine.Efficiency = input[3];
+                }
+                else if (input.Length == 3)
+                {
+                    bool isDigit = int.TryParse(input[2], out var displacement);
+                    if (isDigit)
+                    {
+                        engine.Model = input[0];
+                        engine.Power = int.Parse(input[1]);
+                        engine.Displacement = int.Parse(input[2]);
+                    }
+                    else
+                    {
+                        engine.Model = input[0];
+                        engine.Power = int.Parse(input[1]);
+                        engine.Efficiency = input[2];
 
-            string firstDate = Console.ReadLine();
-            string secondDate = Console.ReadLine();
-            int difference = DateModifier.GetDifferenceBetweenTwoDates(firstDate, secondDate);
-            Console.WriteLine(difference);
+                    }
+                }
+                else
+                {
+                    engine.Model = input[0];
+                    engine.Power = int.Parse(input[1]);
+                }
+                engines.Add(engine.Model, engine);
+            }
+
+            int m = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+            for (int i = 0; i < m; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Car car = new Car();
+                if (input.Length == 4)
+                {
+                    car.Model = input[0];
+                    car.Engine = engines[input[1]];
+                    car.Weight = int.Parse(input[2]);
+                    car.Color = input[3];
+                }
+                else if (input.Length == 3)
+                {
+                    bool isDigit = int.TryParse(input[2], out var displacement);
+                    if (isDigit)
+                    {
+                        car.Model = input[0];
+                        car.Engine = engines[input[1]];
+                        car.Weight = int.Parse(input[2]);
+                    }
+                    else
+                    {
+                        car.Model = input[0];
+                        car.Engine = engines[input[1]];
+                        car.Color = input[2];
+
+                    }
+                }
+                else
+                {
+                    car.Model = input[0];
+                    car.Engine = engines[input[1]];
+                }
+                cars.Add(car);
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Model}:");
+                Console.WriteLine($" {car.Engine.Model}:");
+                Console.WriteLine($"  Power: {car.Engine.Power}");
+                if (car.Engine.Displacement == null)
+                {
+                    Console.WriteLine("  Displacement: n/a");
+                }
+                else
+                {
+                    Console.WriteLine($"  Displacement: {car.Engine.Displacement}");
+                }
+
+                if (car.Engine.Efficiency==null)
+                {
+                    Console.WriteLine("  Efficiency: n/a");
+                }
+                else
+                {
+                    Console.WriteLine($"  Efficiency: {car.Engine.Efficiency}");
+                }
+
+                if (car.Weight==null)
+                {
+                    Console.WriteLine(" Weight: n/a");
+                }
+                else
+                {
+                    Console.WriteLine($" Weight: {car.Weight}");
+                }
+
+                if (car.Color==null)
+                {
+                    Console.WriteLine("Color: n/a");
+                }
+                else
+                {
+                    Console.WriteLine($"Color: {car.Color}");
+                }
+            }
+            
 
         }
+
     }
 }
+
+
+
---
+++ C#Advanced/Defining Classes - Exercise/DefiningClassesOld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Channels;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+        }
+    }
+}
