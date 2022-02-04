7d3c1eec2927c9b7873edea62d89b4a82a43dc72
IvailoSpasov <ivailo.andreev.spasov@gmail.com>
2022-02-04 21:10:47 +0200

    Add Generic exercises

---
+++ C#Advanced/Generics - Lab/GenericScale/EqualityScale.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+   public class EqualityScale<T>
+   {
+       private T _left;
+       private T _right;
+
+       public EqualityScale(T left,T right)
+       {
+           _left = left;
+           _right = right;
+       }
+
+       public bool AreEqual()
+       {
+           return _left.Equals(_right);
+       }
+   }
+}
---
+++ C#Advanced/Generics - Lab/GenericScale/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericScale
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> IsNQqualToM = new EqualityScale<int>(4, 4);
+            Console.WriteLine(IsNQqualToM.AreEqual());
+        }
+    }
+}
