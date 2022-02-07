7a2644d6982ad17934cfab2e4dbc5dc6dfbf1502
IvailoSpasov <ivailo.andreev.spasov@gmail.com>
2022-02-07 20:56:35 +0200

    New exercises

---
+++ C#Advanced/Generics - Exercise/Generics/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Generics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nameAndCityImput = Console.ReadLine().Split();
+
+            string name = $"{nameAndCityImput[0]} {nameAndCityImput[1]}";
+            string street = nameAndCityImput[2];
+            string city = nameAndCityImput[3];
+
+            MyTuple<string, string,string> nameCity = new MyTuple<string, string,string>(name, street,city);
+
+            string[] beerInput = Console.ReadLine().Split();
+
+            string beer = beerInput[0];
+            int beerQuantity = int.Parse(beerInput[1]);
+            string drunkOrNot = beerInput[2];
+            bool IsDrunk = drunkOrNot == "drunk";
+
+            MyTuple<string, int,bool> beerMyTuple = new MyTuple<string, int,bool>(beer, beerQuantity,IsDrunk);
+
+            string[] numbers = Console.ReadLine().Split();
+
+            string accName = numbers[0];
+            double ballance = double.Parse(numbers[1]);
+            string bankName = numbers[2];
+
+            MyTuple<string, double,string> numbersMyTuple = new MyTuple<string, double,string>(accName, ballance,bankName);
+            Console.WriteLine(nameCity);
+            Console.WriteLine(beerMyTuple);
+            Console.WriteLine(numbersMyTuple);
+        }
+    }
+}
