142b5b966711541d9c85dd852735407751814d09
IvailoSpasov <ivailo.andreev.spasov@gmail.com>
2022-02-08 22:23:14 +0200

    new exercises

---
+++ C#Advanced/CSharpAdvancedExam/Birthday Celebration/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Birthday_Celebration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> guests = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            Stack<int> food = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            int wastedFood = 0;
+            while (guests.Count != 0 && food.Count != 0)
+            {
+                int currGuest = guests.Peek();
+                while (currGuest > 0)
+                {
+                    int currFood = food.Pop();
+                    currGuest -= currFood;
+                }
+
+                wastedFood += Math.Abs(currGuest);
+                guests.Dequeue();
+            }
+
+            if (guests.Count == 0)
+            {
+                Console.Write($"Plates: ");
+                for (int i = 0; i < food.Count; i++)
+                {
+                    Console.Write($"{food.Pop()} ");
+                }
+
+                Console.WriteLine();
+
+                Console.WriteLine($"Wasted grams of food: {wastedFood}");
+            }
+            else
+            {
+                Console.WriteLine($"Guests: {string.Join(" ",guests)}");
+                Console.WriteLine($"Wasted grams of food: {wastedFood}");
+            }
+        }
+    }
+}
---
+++ C#Advanced/CSharpAdvancedExam/The Battle of The Five Armies/Program.cs
@@ -0,0 +1,277 @@
+﻿using System;
+
+namespace The_Battle_of_The_Five_Armies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int armyArmor = int.Parse(Console.ReadLine());
+            int numberOfRows = int.Parse(Console.ReadLine());
+            char[][] field = new char[numberOfRows][];
+            int armyRow = 0;
+            int armiCol = 0;
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                string rowsInput = Console.ReadLine().Trim();
+                field[i] = new char[rowsInput.Length];
+                for (int j = 0; j < rowsInput.Length; j++)
+                {
+                    field[i][j] = rowsInput[j];
+                    if (field[i][j] == 'A')
+                    {
+                        armyRow = i;
+                        armiCol = j;
+                    }
+                }
+            }
+
+            string[] input = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+            while (true)
+            {
+                string direction = input[0];
+                int orcsRow = int.Parse(input[1]);
+                int orcsCol = int.Parse(input[2]);
+                field[orcsRow][orcsCol] = 'O';
+
+                if (direction == "up")
+                {
+                    if (IsCordinatesValid(armyRow - 1, armiCol, field))
+                    {
+                        armyArmor -= 1;
+                        if (armyArmor <= 0)
+                        {
+                            field[armyRow][armiCol] = '-';
+                            field[armyRow - 1][armiCol] = 'X';
+                            Console.WriteLine($"The army was defeated at {armyRow - 1};{armiCol}.");
+                            PrintMatrix(field);
+                            return;
+                        }
+                        if (field[armyRow - 1][armiCol] == 'O')
+                        {
+                            armyArmor -= 2;
+                            if (armyArmor <= 0)
+                            {
+                                field[armyRow][armiCol] = '-';
+                                field[armyRow - 1][armiCol] = 'X';
+                                Console.WriteLine($"The army was defeated at {armyRow - 1};{armiCol}.");
+                                PrintMatrix(field);
+                                return;
+                            }
+                            else
+                            {
+                                field[armyRow][armiCol] = '-';
+                                field[armyRow - 1][armiCol] = 'A';
+                                armyRow -= 1;
+                            }
+                        }
+
+                        else if (field[armyRow - 1][armiCol] == 'M')
+                        {
+                            field[armyRow - 1][armiCol] = '-';
+                            field[armyRow][armiCol] = '-';
+                            Console.WriteLine($"The army managed to free the Middle World! Armor left: {armyArmor}");
+                            PrintMatrix(field);
+                            return;
+                        }
+                        else
+                        {
+                            field[armyRow][armiCol] = '-';
+                            field[armyRow - 1][armiCol] = 'A';
+                            armyRow -= 1;
+                        }
+
+                    }
+                    else
+                    {
+                        armyArmor -= 1;
+                    }
+                }
+                else if (direction == "down")
+                {
+                    if (IsCordinatesValid(armyRow + 1, armiCol, field))
+                    {
+                        armyArmor -= 1;
+                        if (armyArmor <= 0)
+                        {
+                            field[armyRow][armiCol] = '-';
+                            field[armyRow + 1][armiCol] = 'X';
+                            Console.WriteLine($"The army was defeated at {armyRow + 1};{armiCol}.");
+                            PrintMatrix(field);
+                            return;
+                        }
+                        if (field[armyRow + 1][armiCol] == 'O')
+                        {
+                            armyArmor -= 2;
+                            if (armyArmor <= 0)
+                            {
+                                field[armyRow][armiCol] = '-';
+                                field[armyRow + 1][armiCol] = 'X';
+                                Console.WriteLine($"The army was defeated at {armyRow + 1};{armiCol}.");
+                                PrintMatrix(field);
+                                return;
+                            }
+                            else
+                            {
+                                field[armyRow][armiCol] = '-';
+                                field[armyRow + 1][armiCol] = 'A';
+                                armyRow += 1;
+                            }
+                        }
+
+                        else if (field[armyRow + 1][armiCol] == 'M')
+                        {
+                            field[armyRow + 1][armiCol] = '-';
+                            field[armyRow][armiCol] = '-';
+                            Console.WriteLine($"The army managed to free the Middle World! Armor left: {armyArmor}");
+                            PrintMatrix(field);
+                            return;
+                        }
+                        else
+                        {
+                            field[armyRow][armiCol] = '-';
+                            field[armyRow + 1][armiCol] = 'A';
+                            armyRow += 1;
+                        }
+
+                    }
+                    else
+                    {
+                        armyArmor -= 1;
+                    }
+                }
+                else if (direction == "left")
+                {
+                    if (IsCordinatesValid(armyRow, armiCol - 1, field))
+                    {
+                        armyArmor -= 1;
+                        if (armyArmor <= 0)
+                        {
+                            field[armyRow][armiCol] = '-';
+                            field[armyRow][armiCol - 1] = 'X';
+                            Console.WriteLine($"The army was defeated at {armyRow};{armiCol - 1}.");
+                            PrintMatrix(field);
+                            return;
+                        }
+                        if (field[armyRow][armiCol - 1] == 'O')
+                        {
+                            armyArmor -= 2;
+                            if (armyArmor <= 0)
+                            {
+                                field[armyRow][armiCol] = '-';
+                                field[armyRow][armiCol - 1] = 'X';
+                                Console.WriteLine($"The army was defeated at {armyRow};{armiCol - 1}.");
+                                PrintMatrix(field);
+                                return;
+                            }
+                            else
+                            {
+                                field[armyRow][armiCol] = '-';
+                                field[armyRow][armiCol - 1] = 'A';
+                                armiCol -= 1;
+                            }
+                        }
+
+                        else if (field[armyRow][armiCol - 1] == 'M')
+                        {
+                            field[armyRow][armiCol - 1] = '-';
+                            field[armyRow][armiCol] = '-';
+                            Console.WriteLine($"The army managed to free the Middle World! Armor left: {armyArmor}");
+                            PrintMatrix(field);
+                            return;
+                        }
+                        else
+                        {
+                            field[armyRow][armiCol] = '-';
+                            field[armyRow][armiCol - 1] = 'A';
+                            armiCol -= 1;
+                        }
+
+                    }
+                    else
+                    {
+                        armyArmor -= 1;
+                    }
+                }
+                else if (direction == "right")
+                {
+                    if (IsCordinatesValid(armyRow, armiCol + 1, field))
+                    {
+                        armyArmor -= 1;
+                        if (armyArmor <= 0)
+                        {
+                            field[armyRow][armiCol] = '-';
+                            field[armyRow][armiCol + 1] = 'X';
+                            Console.WriteLine($"The army was defeated at {armyRow};{armiCol + 1}.");
+                            PrintMatrix(field);
+                            return;
+                        }
+                        if (field[armyRow][armiCol + 1] == 'O')
+                        {
+                            armyArmor -= 2;
+
+                            if (armyArmor <= 0)
+                            {
+                                field[armyRow][armiCol] = '-';
+                                field[armyRow][armiCol + 1] = 'X';
+                                Console.WriteLine($"The army was defeated at {armyRow};{armiCol + 1}.");
+                                PrintMatrix(field);
+                                return;
+                            }
+                            else
+                            {
+                                field[armyRow][armiCol] = '-';
+                                field[armyRow][armiCol + 1] = 'A';
+                                armiCol += 1;
+                            }
+                        }
+
+                        else if (field[armyRow][armiCol + 1] == 'M')
+                        {
+                            field[armyRow][armiCol + 1] = '-';
+                            field[armyRow][armiCol] = '-';
+                            Console.WriteLine($"The army managed to free the Middle World! Armor left: {armyArmor}");
+                            PrintMatrix(field);
+                            return;
+                        }
+                        else
+                        {
+                            field[armyRow][armiCol] = '-';
+                            field[armyRow][armiCol + 1] = 'A';
+                            armiCol += 1;
+                        }
+
+                    }
+                    else
+                    {
+                        armyArmor -= 1;
+                    }
+                }
+
+                
+                input = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+
+        private static void PrintMatrix(char[][] field)
+        {
+            foreach (var row in field)
+            {
+                Console.WriteLine(string.Join("", row));
+            }
+        }
+
+        private static bool IsCordinatesValid(int armyRow, int armiCol, char[][] field)
+        {
+            if (armyRow >= 0 && armyRow < field[0].Length && armiCol >= 0 && armiCol < field.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
