a8635b0105f35ba1cae9fed173d8fbb28705705f
IvailoSpasov <ivailo.andreev.spasov@gmail.com>
2022-02-12 16:02:48 +0200

    New exercises

---
+++ C#Advanced/Iterators and Comparators - Exercise/ComparingObjects/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComparingObjects
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+            HashSet<Person> hashSet = new HashSet<Person>();
+            SortedSet<Person> srtedSet = new SortedSet<Person>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split();
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+                Person person = new Person(name, age);
+                hashSet.Add(person);
+                srtedSet.Add(person);
+            }
+
+            Console.WriteLine(hashSet.Count);
+            Console.WriteLine(srtedSet.Count);
+        }
+    }
+}
---
+++ C#Advanced/Iterators and Comparators - Exercise/Froggy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Froggy
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<int> stones = new List<int>(Console.ReadLine().Split(", ").Select(int.Parse));
+            Lake lake = new Lake(stones);
+            Console.WriteLine(string.Join(", ",lake));
+        }
+    }
+}
