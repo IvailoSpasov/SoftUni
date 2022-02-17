6755a9c2e9d62350eca70880a6295105499d3988
IvailoSpasov <ivailo.andreev.spasov@gmail.com>
2022-02-17 20:54:26 +0200

    Add new exercises

---
+++ C#Advanced/CSharpAdvancedExam/Cooking/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+namespace Cooking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> liquidsQueue = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            Stack<int> ingrediantsStack = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            Dictionary<string, int> foods = new Dictionary<string, int>();
+
+            foods.Add("Bread", 0);
+            foods.Add("Cake", 0);
+            foods.Add("Pastry", 0);
+            foods.Add("Fruit Pie", 0);
+
+            while (liquidsQueue.Count > 0 && ingrediantsStack.Count > 0)
+            {
+                int foodValue = ingrediantsStack.Peek() + liquidsQueue.Dequeue();
+                if (foodValue == 25 || foodValue == 50 || foodValue == 75 || foodValue == 100)
+                {
+
+                    string food = String.Empty;
+                    switch (foodValue)
+                    {
+                        case 25:
+                            food = "Bread";
+                            break;
+                        case 50:
+                            food = "Cake";
+                            break;
+                        case 75:
+                            food = "Pastry";
+                            break;
+                        case 100:
+                            food = "Fruit Pie";
+                            break;
+                    }
+
+                    ingrediantsStack.Pop();
+                    foods[food]++;
+                }
+                else
+                {
+                    ingrediantsStack.Push(ingrediantsStack.Pop() + 3);
+                }
+            }
+
+            if (foods.Where(x => x.Value > 0).Count() == 4)
+            {
+                Console.WriteLine("Wohoo! You succeeded in cooking all the food!");
+            }
+            else
+            {
+                Console.WriteLine("Ugh, what a pity! You didn't have enough materials to cook everything.");
+            }
+
+            if (liquidsQueue.Count>0)
+            {
+                Console.WriteLine($"Liquids left: {string.Join(", ",liquidsQueue)}");
+            }
+            else
+            {
+                Console.WriteLine("Liquids left: none");
+            }
+
+            if (ingrediantsStack.Count>0)
+            {
+                Console.WriteLine($"Ingredients left: {string.Join(", ",ingrediantsStack)}");
+            }
+            else
+            {
+                Console.WriteLine("Ingredients left: none");
+            }
+
+            foreach (var food in foods.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{food.Key}: {food.Value}");
+            }
+        }
+    }
+}
---
+++ C#Advanced/CSharpAdvancedExam/Selling/Program.cs
@@ -0,0 +1,263 @@
+﻿using System;
+
+namespace Selling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] bakery = new char[n, n];
+
+            int playerRow = 0;
+            int playerCol = 0;
+            int firstMirorRow = -1;
+            int firstMirorCol = -1;
+            int secondMirorRow = -1;
+            int secondMirorCol = -1;
+
+            for (int row = 0; row < n; row++)
+            {
+                string cols = Console.ReadLine();
+                for (int col = 0; col < n; col++)
+                {
+                    bakery[row, col] = cols[col];
+                    if (bakery[row, col] == 'S')
+                    {
+                        playerRow = row;
+                        playerCol = col;
+                    }
+
+                    if (bakery[row, col] == 'O' && firstMirorRow == -1)
+                    {
+                        firstMirorRow = row;
+                        firstMirorCol = col;
+                    }
+                    else if (bakery[row, col] == 'O' && firstMirorRow > -1)
+                    {
+                        secondMirorRow = row;
+                        secondMirorCol = col;
+                    }
+                }
+            }
+
+            int money = 0;
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "up")
+                {
+                    if (IsCordinatesValid(playerRow - 1, playerCol, n))
+                    {
+
+                        bakery[playerRow, playerCol] = '-';
+                        playerRow--;
+
+                        if (char.IsDigit(bakery[playerRow, playerCol]))
+                        {
+                            int value = int.Parse(bakery[playerRow, playerCol].ToString());
+                            money += value;
+                            bakery[playerRow, playerCol] = 'S';
+                        }
+                        else if (bakery[playerRow, playerCol] == 'O')
+                        {
+                            if (playerRow == firstMirorRow && playerCol == firstMirorCol)
+                            {
+                                bakery[playerRow, playerCol] = '-';
+                                playerRow = secondMirorRow;
+                                playerCol = secondMirorCol;
+                                bakery[playerRow, playerCol] = 'S';
+                            }
+                            else
+                            {
+                                bakery[playerRow, playerCol] = '-';
+                                playerRow = firstMirorRow;
+                                playerCol = firstMirorCol;
+                                bakery[playerRow, playerCol] = 'S';
+                            }
+                        }
+                        else
+                        {
+                            bakery[playerRow, playerCol] = 'S';
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bad news, you are out of the bakery.");
+                        Console.WriteLine($"Money: {money}");
+                        bakery[playerRow, playerCol] = '-';
+                        PrintingMatrix(bakery);
+                        break;
+                    }
+                }
+                else if (command == "down")
+                {
+                    if (IsCordinatesValid(playerRow + 1, playerCol, n))
+                    {
+
+                        bakery[playerRow, playerCol] = '-';
+                        playerRow++;
+
+                        if (char.IsDigit(bakery[playerRow, playerCol]))
+                        {
+                            int value = int.Parse(bakery[playerRow, playerCol].ToString());
+                            money += value;
+                            bakery[playerRow, playerCol] = 'S';
+                        }
+                        else if (bakery[playerRow, playerCol] == 'O')
+                        {
+                            if (playerRow == firstMirorRow && playerCol == firstMirorCol)
+                            {
+                                bakery[playerRow, playerCol] = '-';
+                                playerRow = secondMirorRow;
+                                playerCol = secondMirorCol;
+                                bakery[playerRow, playerCol] = 'S';
+                            }
+                            else
+                            {
+                                bakery[playerRow, playerCol] = '-';
+                                playerRow = firstMirorRow;
+                                playerCol = firstMirorCol;
+                                bakery[playerRow, playerCol] = 'S';
+                            }
+                        }
+                        else
+                        {
+                            bakery[playerRow, playerCol] = 'S';
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bad news, you are out of the bakery.");
+                        Console.WriteLine($"Money: {money}");
+                        bakery[playerRow, playerCol] = '-';
+                        PrintingMatrix(bakery);
+                        break;
+                    }
+                }
+                else if (command == "left")
+                {
+                    if (IsCordinatesValid(playerRow, playerCol - 1, n))
+                    {
+
+                        bakery[playerRow, playerCol] = '-';
+                        playerCol--;
+
+                        if (char.IsDigit(bakery[playerRow, playerCol]))
+                        {
+                            int value = int.Parse(bakery[playerRow, playerCol].ToString());
+                            money += value;
+                            bakery[playerRow, playerCol] = 'S';
+                        }
+                        else if (bakery[playerRow, playerCol] == 'O')
+                        {
+                            if (playerRow == firstMirorRow && playerCol == firstMirorCol)
+                            {
+                                bakery[playerRow, playerCol] = '-';
+                                playerRow = secondMirorRow;
+                                playerCol = secondMirorCol;
+                                bakery[playerRow, playerCol] = 'S';
+                            }
+                            else
+                            {
+                                bakery[playerRow, playerCol] = '-';
+                                playerRow = firstMirorRow;
+                                playerCol = firstMirorCol;
+                                bakery[playerRow, playerCol] = 'S';
+                            }
+                        }
+                        else
+                        {
+                            bakery[playerRow, playerCol] = 'S';
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bad news, you are out of the bakery.");
+                        Console.WriteLine($"Money: {money}");
+                        bakery[playerRow, playerCol] = '-';
+                        PrintingMatrix(bakery);
+                        break;
+                    }
+                }
+                else if (command == "right")
+                {
+                    if (IsCordinatesValid(playerRow, playerCol + 1, n))
+                    {
+
+                        bakery[playerRow, playerCol] = '-';
+                        playerCol++;
+
+                        if (char.IsDigit(bakery[playerRow, playerCol]))
+                        {
+                            int value = int.Parse(bakery[playerRow, playerCol].ToString());
+                            money += value;
+                            bakery[playerRow, playerCol] = 'S';
+                        }
+                        else if (bakery[playerRow, playerCol] == 'O')
+                        {
+                            if (playerRow == firstMirorRow && playerCol == firstMirorCol)
+                            {
+                                bakery[playerRow, playerCol] = '-';
+                                playerRow = secondMirorRow;
+                                playerCol = secondMirorCol;
+                                bakery[playerRow, playerCol] = 'S';
+                            }
+                            else
+                            {
+                                bakery[playerRow, playerCol] = '-';
+                                playerRow = firstMirorRow;
+                                playerCol = firstMirorCol;
+                                bakery[playerRow, playerCol] = 'S';
+                            }
+                        }
+                        else
+                        {
+                            bakery[playerRow, playerCol] = 'S';
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bad news, you are out of the bakery.");
+                        Console.WriteLine($"Money: {money}");
+                        bakery[playerRow, playerCol] = '-';
+                        PrintingMatrix(bakery);
+                        break;
+                    }
+
+                }
+
+                if (money>=50)
+                {
+                    Console.WriteLine("Good news! You succeeded in collecting enough money!");
+                    Console.WriteLine($"Money: {money}");
+                    PrintingMatrix(bakery);
+                    break;
+                }
+            }
+        }
+
+        private static void PrintingMatrix(char[,] bakery)
+        {
+            for (int row = 0; row < bakery.GetLength(0); row++)
+            {
+                for (int col = 0; col < bakery.GetLength(1); col++)
+                {
+                    Console.Write(bakery[row,col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static bool IsCordinatesValid(int playerRow, int playerCol, int i)
+        {
+            if (playerRow >= 0 && playerRow < i && playerCol >= 0 && playerCol < i)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
