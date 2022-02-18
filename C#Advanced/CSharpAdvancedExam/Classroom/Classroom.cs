af9415b87ad6b52462c5af5c17807f0504c1646e
IvailoSpasov <ivailo.andreev.spasov@gmail.com>
2022-02-18 21:18:03 +0200

    Added new exercises

---
+++ C#Advanced/CSharpAdvancedExam/Classroom/Classroom.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class Classroom
+    {
+        private List<Student> students;
+
+        public Classroom(int capacity)
+        {
+            Capacity = capacity;
+            students = new List<Student>();
+        }
+
+        public int Capacity { get; set; }
+
+        public int Count => students.Count;
+
+        public string RegisterStudent(Student student)
+        {
+            if (students.Count < Capacity)
+            {
+                students.Add(student);
+                return $"Added student {student.FirstName} {student.LastName}";
+            }
+            else
+            {
+                return "No seats in the classroom";
+            }
+        }
+
+        public string DismissStudent(string firstName, string lastName)
+        {
+            Student student = students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+
+            if (student != null)
+            {
+                students.Remove(student);
+                return $"Dismissed student {firstName} {lastName}";
+            }
+            else
+            {
+                return "Student not found";
+            }
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+            if (students.Any(x => x.Subject == subject))
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine($"Subject: {subject}\nStudents:");
+                foreach (var student in students.Where(x => x.Subject == subject))
+                {
+                    sb.AppendLine($"{student.FirstName} {student.LastName}");
+                }
+
+                return sb.ToString().TrimEnd();
+            }
+            else
+            {
+                return "No students enrolled for the subject";
+            }
+        }
+
+        public Student GetStudent(string firstName, string lastName)
+        {
+            Student student = students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+            return student;
+        }
+    }
+}
---
+++ C#Advanced/CSharpAdvancedExam/Garden/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Garden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+            int[,] garden = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    garden[i, j] = 0;
+                }
+            }
+
+            string cordinates = Console.ReadLine();
+            List<int[]> fwolers = new List<int[]>();
+            while (cordinates != "Bloom Bloom Plow")
+            {
+                int[] splited = cordinates.Split().Select(int.Parse).ToArray();
+                int row = splited[0];
+                int col = splited[1];
+
+                if (row >= 0 && row < rows && col >= 0 && col < cols)
+                {
+                    fwolers.Add(new int[2] { row, col });
+                }
+                else
+                {
+                    Console.WriteLine("Invalid coordinates.");
+                }
+
+                cordinates = Console.ReadLine();
+            }
+
+            for (int i = 0; i < fwolers.Count; i++)
+            {
+                int row = fwolers[i][0];
+                int col = fwolers[i][1];
+                garden[row, col]++;
+
+                for (int j = 0; j < cols; j++)
+                {
+                    if (j != col)
+                    {
+                        garden[row, j]++;
+                    }
+                }
+
+                for (int k = 0; k < rows; k++)
+                {
+                    if (k != row)
+                    {
+                        garden[k, col]++;
+                    }
+                }
+            }
+
+            for (int rowsIndex = 0; rowsIndex < rows; rowsIndex++)
+            {
+                for (int colIndex = 0; colIndex < cols; colIndex++)
+                {
+                    Console.Write($"{garden[rowsIndex, colIndex]} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
