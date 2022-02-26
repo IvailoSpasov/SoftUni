41bf54ca1745a80b72ababef29e8f4a4a4d393ac
IvailoSpasov <ivailo.andreev.spasov@gmail.com>
2022-02-26 12:55:08 +0200

    Added new exercises

---
+++ C#OOP/Inheritance - Exercise/Animals/StartUp.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+
+            while (true)
+            {
+                string type = Console.ReadLine();
+                if (type == "Beast!")
+                {
+                    break;
+                }
+
+                string[] animalData = Console.ReadLine().Split();
+                string name = animalData[0];
+                int age = int.Parse(animalData[1]);
+                string gender = animalData[1];
+
+                if (age < 0)
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+
+                Animal animal = null;
+
+                if (type == "Cat")
+                {
+                    animal = new Cat(name, age, gender);
+                }
+                else if (type == "Dog")
+                {
+                    animal = new Dog(name, age, gender);
+                }
+                else if (type == "Frog")
+                {
+                    animal = new Frog(name, age, gender);
+                }
+                else if (type == "Kitten")
+                {
+                    animal = new Kitten(name, age);
+                }
+                else
+                {
+                    animal = new Tomcat(name, age);
+                }
+                
+                Console.WriteLine(type);
+                Console.WriteLine($"{animal.Name} {animal.Age} {animal.Gender}");
+                Console.WriteLine(animal.ProduceSound());
+                
+            }
+
+        }
+    }
+}
---
+++ C#OOP/Inheritance - Exercise/NeedForSpeed/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SportCar sportcar = new SportCar(300, 100);
+            sportcar.Drive(10);
+            Console.WriteLine(sportcar.Fuel);
+        }
+    }
+}
