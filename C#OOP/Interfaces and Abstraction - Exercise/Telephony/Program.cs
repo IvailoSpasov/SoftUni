442075769a9bb4399f00b19bc6512a9025f99163
IvailoSpasov <ivailo.andreev.spasov@gmail.com>
2022-03-03 17:33:58 +0200

    Added new exercises

---
+++ C#OOP/Interfaces and Abstraction - Exercise/Telephony/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Telephony
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine().Split();
+            string[] urls = Console.ReadLine().Split();
+
+            Smartphone phone = new Smartphone();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                phone.Call(numbers[i]);
+            }
+
+            for (int i = 0; i < urls.Length; i++)
+            {
+                phone.Browse(urls[i]);
+            }
+            
+        }
+    }
+}
---
+++ C#OOP/Interfaces and Abstraction - Exercise/Telephony/Smartphone.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telephony
+{
+    public class Smartphone : ICallable, IBrowseble
+    {
+        public void Call(string number)
+        {
+            if (number.Any(x => char.IsLetter(x)))
+            {
+                Console.WriteLine("Invalid number!");
+            }
+            else if (number.Length == 10)
+            {
+                Console.WriteLine($"Calling... {number}");
+            }
+            else
+            {
+                Console.WriteLine($"Dialing... {number}");
+            }
+        }
+
+        public void Browse(string url)
+        {
+            if (url.Any(x => char.IsDigit(x)))
+            {
+                Console.WriteLine("Invalid URL!");
+            }
+            else
+            {
+                Console.WriteLine($"Browsing: {url}!");
+            }
+        }
+    }
+}
