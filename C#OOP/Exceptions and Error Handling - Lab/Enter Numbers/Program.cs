c06d717b59c2c493b5971b59d3291b7c2b3d3893
IvailoSpasov <ivailo.andreev.spasov@gmail.com>
2022-03-12 15:46:21 +0200

    Added new exercises

---
+++ C#OOP/Exceptions and Error Handling - Lab/Enter Numbers/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Enter_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingNumber = 1;
+            int finalNumber = 100;
+            int[] inputNumbers = new int[10];
+
+            for (int i = 0; i < inputNumbers.Length; i++)
+            {
+                try
+                {
+                    inputNumbers[i] = ReadNumber(startingNumber, finalNumber);
+
+
+                    if (inputNumbers[i] <= startingNumber || inputNumbers[i] > 100)
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter a valid number");
+                    i--;
+                    continue;
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("Number must be bigger than {0} and smaller than {1}!", startingNumber, finalNumber);
+                    i--;
+                    continue;
+                }
+
+
+                startingNumber = inputNumbers[i];
+            }
+
+            Console.Write("Your numbers are: ");
+            foreach (var n in inputNumbers)
+            {
+                Console.Write(n + ", ");
+            }
+            Console.WriteLine();
+
+        }
+        public static int ReadNumber(int start, int end)
+        {
+            string input = Console.ReadLine();
+            int num;
+
+            while (!int.TryParse(input, out num))
+            {
+                throw new FormatException();
+            }
+
+
+            return num;
+        }
+    }
+    
+}
---
+++ C#OOP/Exceptions and Error Handling - Lab/Square Root/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Square_Root
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double number = double.Parse(Console.ReadLine());
+                double sqrt = Math.Sqrt(number);
+                if (double.IsNaN(sqrt))
+                {
+                    throw new ArgumentException("Invalid number.");
+                }
+                Console.WriteLine(sqrt);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+
+        
+    }
+}
