7166a69810dc1b224c93fbcea1bb335e67f4bd23
IvailoSpasov <ivailo.andreev.spasov@gmail.com>
2022-03-19 11:49:47 +0200

    Added new exercises

---
+++ C#OOP/ReflectionAndAttributes - Exercise/ValidationAttributes/Attributes/MyRequiredAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace ValidationAttributes.Attributes
+{
+    public class MyRequiredAttribute : MyValidationAttribute
+    {
+        public override bool IsValid(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            return true;
+        }
+    }
+}
