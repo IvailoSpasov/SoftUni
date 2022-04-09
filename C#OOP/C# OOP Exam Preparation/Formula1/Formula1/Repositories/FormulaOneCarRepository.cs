956467316a3008e04a46acbfa41d94d0991b2bba
IvailoSpasov <ivailo.andreev.spasov@gmail.com>
2022-04-09 12:48:52 +0300

    Added new exercises

---
+++ C#OOP/C# OOP Exam Preparation/Formula1/Formula1/Repositories/FormulaOneCarRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
+
+namespace Formula1.Repositories
+{
+    public class FormulaOneCarRepository : IRepository<IFormulaOneCar>
+    {
+        private List<IFormulaOneCar> models;
+
+        public FormulaOneCarRepository()
+        {
+            this.models = new List<IFormulaOneCar>();
+        }
+
+        public IReadOnlyCollection<IFormulaOneCar> Models => this.models;
+
+        public void Add(IFormulaOneCar model)
+        {
+            this.models.Add(model);
+        }
+
+        public bool Remove(IFormulaOneCar model)
+        {
+            return this.models.Remove(model);
+        }
+
+        public IFormulaOneCar FindByName(string name)
+        {
+            IFormulaOneCar car = models.FirstOrDefault(x => x.Model == name);
+            return car;
+        }
+    }
+}
